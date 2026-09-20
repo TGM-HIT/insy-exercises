56e4d84e147f1f0f48393f1524a0f07e81b1c289
Dominik <dominik.hoebert@me.com>
2026-09-20 16:38:33 +0200

    Refactor iframe-resize.js for improved readability and formatting

--- docs/iframe-resize.js
+++ docs/iframe-resize.js
@@ -1,7 +1,19 @@
-document$.subscribe(function() {
+document$.subscribe(function () {
   function sendHeight() {
-    window.parent.postMessage({ height: document.documentElement.scrollHeight }, '*');
+    window.parent.postMessage(
+      { height: document.documentElement.scrollHeight },
+      "*",
+    );
   }
+
   sendHeight();
   new ResizeObserver(sendHeight).observe(document.body);
+
+  // Instant update on admonition open/close
+  document.querySelectorAll("details").forEach((el) => {
+    el.addEventListener("toggle", () => {
+      // let the DOM settle first
+      requestAnimationFrame(sendHeight);
+    });
+  });
 });
